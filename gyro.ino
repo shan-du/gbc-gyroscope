98f116695852a10eb5caa18660f265737d9f3c6e
Shan <shan-du@users.noreply.github.com>
2019-12-06 12:28:15 -0500

    Update encoder ID to base on 0, 5000, 10000

--- gyro.ino
+++ gyro.ino
@@ -12,7 +12,7 @@ int outputAPreviousState[numberOfEncoders];
 
 int outputA[] = {output1A, output2A, output3A};
 int outputB[] = {output1B, output2B, output3B};
-long basePadding = 1000000;
+int basePadding = 1000;
 
 void setup() { 
   pinMode (output1A,INPUT);
@@ -45,8 +45,8 @@ void encoderLoop(int index) {
    // Compose the message for printing
    // String message = "Encoder " + (String)index + ": " + (String)counter[index];
    // pad encoder output with big number to identify each encoder
-   // e.g encoder 1: 1000000, encoder 2: 5000000, encoder 3: 9000000
-   long paddedOutput = basePadding * (index * 4 + 1) + counter[index];
+   // e.g encoder 1: 0, encoder 2: 5000, encoder 3: 10000
+   int paddedOutput = basePadding * (index * 5) + counter[index];
    Serial.println(paddedOutput);
   }
   
